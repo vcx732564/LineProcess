1eb3cf962f151cf277a5a945f8ffd7ed86960762
vcx732564 <vcx732564@hotmail.com>
2018-05-22 22:13:53 +0800

    2018-05-22 1.更新遊戲清單指令 2.增加RPaLibrary 3.增加DB連線

--- MyLineBot/Controllers/LineWebHookSampleController.cs
+++ MyLineBot/Controllers/LineWebHookSampleController.cs
@@ -6,7 +6,10 @@ using System.Net.Http;
 using System.Web.Http;
 using System.Text;
 
+using System.IO;
 using Newtonsoft.Json;
+using System.Web;
+using System.Data;
 
 namespace MyLineBot.Controllers
 {
@@ -35,28 +38,39 @@ namespace MyLineBot.Controllers
                     {
                         if (LineEvent.message.text.Substring(0, 1) == "!")
                         {
-                            switch (LineEvent.message.text.Replace("!", ""))
+
+                            try
                             {
-                                //換行符號 %0D%0A %0D%0A 
-                                case "本月遊戲清單":
-                                    
-                                    
-                                    string MyReply = "本月遊戲清單:  \n PS4 - 拉比哩比 (2018-05-08) ";
-                                    MyReply += " \n PS4 - H1Z1 屍流感 (2018-05-22) ";
-                                    MyReply += " \n PS4 - 女神異聞錄 5 星夜熱舞  (2018-05-25) ";
 
-                                    this.ReplyMessage(LineEvent.replyToken, MyReply);
+                                if (LineEvent.message.text.Split('!').Length == 2)
+                                {
 
-                                    break;
-                                case "一哥廢物":
-                                    this.ReplyMessage(LineEvent.replyToken, "說得好R!");
+                                    string sReplyString = OneCommand(LineEvent.message.text);
+                                    this.ReplyMessage(LineEvent.replyToken, sReplyString);
 
-                                    break;
+                                }
+                                else if(LineEvent.message.text.Split('!').Length == 3)
+                                {
 
-                                default:
-                                    this.ReplyMessage(LineEvent.replyToken, "安安你說了:" + LineEvent.message.text + "，窩抗不懂^_^");
-                                    break;
+                                    string sReplyString = twoCommand(LineEvent.message.text);
+                                    this.ReplyMessage(LineEvent.replyToken, sReplyString);
+
+                                }
+                                else
+                                {
+
+                                    this.ReplyMessage(LineEvent.replyToken, "指令不正確");
+                                }
+
+
+                                
                             }
+                            catch (Exception ex)
+                            {
+
+                                this.ReplyMessage(LineEvent.replyToken, "執行指令發生錯誤：" + ex.Message);
+                            }
+
                         }
                     }
 
@@ -78,5 +92,163 @@ namespace MyLineBot.Controllers
                 return Ok();
             }
         }
+
+
+        private string OneCommand(string Command)
+        {
+            string ResultString = "";
+            switch (Command)
+            {
+                //換行符號 %0D%0A %0D%0A 
+                case "!本月遊戲清單":
+                    ResultString = getGameList("");
+                    
+                    break;
+                case "!一哥廢物":
+
+                    ResultString = "說得好^_^";
+                    break;
+
+                default:
+                    ResultString = "安安你說了:" + Command + "，窩抗不懂^_^";
+                    break;
+            }
+
+            return ResultString;
+        }
+
+
+        private string twoCommand(string Command)
+        {
+
+            string ResultString = "";
+            //0是沒有資料的 1是指令 2是值
+            string[] arrCommond = Command.Split('!');
+
+            switch (arrCommond[1])
+            {
+                //換行符號 %0D%0A %0D%0A 
+                case "本月遊戲清單":
+                    int iMonth = 0;
+                    if (int.TryParse(arrCommond[2],out iMonth))
+                    {
+                        if (iMonth >=1 && iMonth <= 12)
+                        {
+                            ResultString = getGameList(iMonth.ToString("00"));
+                        }
+                        else
+                        {
+                            ResultString = "指令格式輸入錯誤,月份只能輸入1~12月份";
+                        }
+
+                    }
+                    else
+                    {
+                        ResultString = "指令格式輸入錯誤,指令為：\n !本月遊戲清單!NN \n NN為1~12月份" ;
+                    }
+
+
+                    break;
+
+
+                default:
+                    ResultString = "安安你說了:" + Command + "，窩抗不懂^_^";
+                    break;
+            }
+
+
+            return ResultString;
+        }
+
+
+        private string getGameList(string AssignMonth)
+        {
+            if(AssignMonth == "")
+            {
+                AssignMonth = DateTime.Now.ToString("yyyy/MM");
+            }
+            else
+            {
+                AssignMonth = DateTime.Now.ToString("yyyy/") + AssignMonth;
+            }
+
+
+            string LastUpdateTime = "最後更新日期：2018/05/22 ";
+            string sMyReply = "本月遊戲清單\n";
+
+            RPaWorkLibrary.MSSQL RM = new RPaWorkLibrary.MSSQL();
+            RPaWorkLibrary.Encryption RE = new RPaWorkLibrary.Encryption();
+            DataTable dtGameList;
+
+            try
+            {
+   
+                RM.ServerName = RE.EnCodeString(System.Configuration.ConfigurationManager.AppSettings["DBServer"]);
+                RM.DataBaseName = RE.EnCodeString(System.Configuration.ConfigurationManager.AppSettings["DBDataBase"]);
+                RM.DB_Id = RE.EnCodeString(System.Configuration.ConfigurationManager.AppSettings["DBId"]);
+                RM.DB_Psw = RE.EnCodeString(System.Configuration.ConfigurationManager.AppSettings["DBPws"]);
+
+                try
+                {
+                    
+                    DateTime LastDay = Convert.ToDateTime(AssignMonth + "/01").AddMonths(1).AddDays(-Convert.ToDateTime(AssignMonth + "/01").AddMonths(1).Day);
+
+                    string WhereString = " AND dSaleDate >= '" + AssignMonth + "/01' ";
+                    WhereString += " AND dSaleDate <= '" + AssignMonth + "/" + LastDay.ToString("dd") + "' ";
+                    dtGameList = RM.Get_DataTable("SELECT * FROM GameList WHERE 1 = 1 " + WhereString);
+
+                    if (dtGameList.Rows.Count == 0)
+                    {
+                        return "此月無發售遊戲(" + LastUpdateTime + ")";
+
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    throw new Exception("【撈資料發生問題】" + ex.Message);
+                }
+
+
+                try
+                {
+
+                    foreach (DataRow dr in dtGameList.Rows)
+                    {
+                        string sLanguage = "";
+                        string sSaleDate = dr["dSaleDate"].ToString();
+
+                        if(dr["sLanguage"].ToString().Trim() != "中")
+                        {
+                            sLanguage = "(" + dr["sLanguage"].ToString().Trim() + ")";
+                        }
+                       
+                        sMyReply += Convert.ToDateTime(sSaleDate).ToString("yyyy/MM/dd") +  " " + dr["sName"].ToString().TrimEnd()+ sLanguage + " \n";
+
+                    }
+
+                    return sMyReply + LastUpdateTime ;
+                }
+                catch (Exception ex)
+                {
+
+                    throw new Exception("【組合文字訊息發生錯誤】" + ex.Message);
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("本月遊戲清單執行錯誤 - " + ex.Message);
+            }
+
+
+            
+        }
+
+
+
+
     }
 }
